ee1c85558e34adfe1e32efce80c0a5c3171a1108
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-09-05 23:50:20 +0300

    transactions

--- project/applications/finances/admin.py
+++ project/applications/finances/admin.py
@@ -3,20 +3,6 @@ from .models import Wallet, Transaction
 
 # Register your models here.
 
-class FromTransactionInline(admin.StackedInline):
-    model = Transaction
-    fk_name = 'from_wallet'
 
-
-class ToTransactionInline(admin.StackedInline):
-    model = Transaction
-    fk_name = 'to_wallet'
-
-
-class WalletAdmin(admin.ModelAdmin):
-    model = Wallet
-
-    inlines = [FromTransactionInline, ToTransactionInline, ]
-
-
-admin.site.register(Wallet, WalletAdmin)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
